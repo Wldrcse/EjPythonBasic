8a5ab350337721805b68b5bf0ed160983f9c509d
Wldrcse <101368673+Wldrcse@users.noreply.github.com>
2024-04-18 17:33:58 -0500

    A

--- Datos-IMC.py
+++ Datos-IMC.py
@@ -3,5 +3,3 @@ peso = float(input("Ingrese su peso en Kg: "))
 talla = float(input("Ingrese su talla en m: "))
 imc = peso/(talla**2)
 print("Su Índice de Masa Corporal es: {0:.2f}".format(imc)) # Mostrar solo 2 decimales
-
-##
