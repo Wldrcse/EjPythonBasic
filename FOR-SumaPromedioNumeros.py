6a26c6a82a3ec899d445a0f7496914ef6f82e660
Wi <wldrc.se@gmail.com>
2024-04-23 22:52:06 -0500

    Update FOR-SumaPromedioNumeros.py

--- FOR-SumaPromedioNumeros.py
+++ FOR-SumaPromedioNumeros.py
@@ -4,5 +4,9 @@ suma = 0
 for i in range(n):
     num = float(input("Ingrese número: "))
     suma = suma + num
+# Versión 1 para mostrar
 print("La suma de los números ingresados es {0:.2f}".format(suma))
 print("El promedio de los números ingresados es {0:.2f}".format(suma/n))
+# Versión 1 para mostrar
+print("La suma de los números ingresados es {:.2f}".format(suma))
+print("El promedio de los números ingresados es {:.2f}".format(suma/n))
